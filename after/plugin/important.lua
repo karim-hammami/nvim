db9ff9970091a505ee27a491419acdedbe68fa4b
karim-hammami <hammamik790@gmail.com>
2023-01-24 01:14:36 +0100

    quality improvement

--- after/plugin/important.lua
+++ after/plugin/important.lua
@@ -1,1 +1,8 @@
+-- update packer
 vim.keymap.set("n", "<leader>su", vim.cmd.PackerSync)
+-- vertical split
+vim.keymap.set("n", "<C-a>", vim.cmd.vsplit)
+-- horizontal split
+vim.keymap.set("n", "<C-y>", vim.cmd.split)
+
+
