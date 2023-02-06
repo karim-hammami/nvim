0e40550cb03e1e70c0ac46d2d46652075cbc9df5
karim-hammami <hammamik790@gmail.com>
2023-02-06 23:41:12 +0100

    prevent transparency in nvim

--- after/plugin/important.lua
+++ after/plugin/important.lua
@@ -7,3 +7,6 @@ vim.keymap.set("n", "<C-y>", vim.cmd.split)
 -- run mason lsp
 
 vim.cmd("nmap <C-i> mzgg=G`z")
+
+vim.cmd("highlight Normal ctermbg=Black")
+vim.cmd("highlight NonText ctermbg=Black")
