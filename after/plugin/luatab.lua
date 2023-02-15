e08875328d1bb3014857d64abd5f57894c7fae8f
karim-hammami <hammamik790@gmail.com>
2023-02-16 00:35:39 +0100

    set winbar

--- after/plugin/luatab.lua
+++ after/plugin/luatab.lua
@@ -13,8 +13,7 @@ require('tabline').setup({
 })
 
 
-
+vim.cmd("set winbar=%t")
 vim.keymap.set("n", "<A-tab>", vim.cmd.tabnext )
 vim.keymap.set("n", "<leader>t", vim.cmd.tabnew )
-vim.keymap.set("n", "<C-w>", vim.cmd.tabclose )
 
