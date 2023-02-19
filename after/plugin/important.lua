cf6ce70ea93037d8cd200b0cf0a06de431e85af7
karim-hammami <hammamik790@gmail.com>
2023-02-19 13:42:14 +0100

    adding selector.nvim keybind

--- after/plugin/important.lua
+++ after/plugin/important.lua
@@ -4,13 +4,12 @@ vim.keymap.set("n", "<leader>su", vim.cmd.PackerSync)
 vim.keymap.set("n", "<C-a>", vim.cmd.vsplit)
 -- horizontal split
 vim.keymap.set("n", "<C-y>", vim.cmd.split)
--- select all 
+-- select all
 vim.keymap.set("n", "<leader>a", "gg<S-v>G")
 -- sync yank behaviour to clipboard (require xclip for x11)
 vim.keymap.set("n", "y", '"+y')
 vim.keymap.set("n", "yy", '"+yy')
 vim.keymap.set("v", "y", '"+y')
-
+vim.keymap.set("n", "<A-e>", require "selector".select)
 
 vim.cmd("nmap <C-i> mzgg=G`z")
-
